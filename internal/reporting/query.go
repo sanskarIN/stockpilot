bd1439bec8d8fcce22365efc8b759aacd98287fe
Sanskar <sanskarin@outlook.in>
2026-09-04 19:55:44 +0530

    fix(reporting): import time for query options

--- internal/reporting/query.go
+++ internal/reporting/query.go
@@ -1,5 +1,7 @@
 package reporting
 
+import "time"
+
 // Query describes the bounded portion of a report requested by a caller.
 // From and To are optional for reports that are not time-based.
 type Query struct {
