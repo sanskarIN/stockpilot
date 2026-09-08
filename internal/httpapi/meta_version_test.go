e0c337525ae97759da5cc40f7cdd01edc5db0435
Sanskar <sanskarin@outlook.in>
2026-09-08 16:34:18 +0530

    test(api): align meta version regression with 0.5.2

--- internal/httpapi/meta_version_test.go
+++ internal/httpapi/meta_version_test.go
@@ -24,7 +24,7 @@ func TestMetaEndpointReportsReleaseVersion(t *testing.T) {
 	if response.Name != "StockPilot" {
 		t.Fatalf("name=%q", response.Name)
 	}
-	if response.Version != "0.5.1" {
+	if response.Version != "0.5.2" {
 		t.Fatalf("version=%q", response.Version)
 	}
 }
